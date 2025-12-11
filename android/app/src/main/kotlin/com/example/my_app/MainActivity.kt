70272cd36a02b2bacb15ab8450945171c2557be2
alexkmsistemas <alex@kmsistemas.com.br>
2025-12-11 09:23:03 -0300

    Deploy web build to root

--- android/app/src/main/kotlin/com/example/my_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.KmIndustrial
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "6628c79c0528b00b81e783cbc89d5ca4
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ecd18e41fef3f926a6559622c861be82",
+"flutter_bootstrap.js": "69b3dc6cb4423b348aa89f80c8ee4517",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
